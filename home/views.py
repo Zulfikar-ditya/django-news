058d4793f541c2a87b4dfe881e55816dd01534e1
Zulfikar-ditya <zulfikarditya.work@gmail.com>
2020-10-06 20:07:49 +0700

    hello world in web page, add bootstrap, etc

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'home/index.html')
